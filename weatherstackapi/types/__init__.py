a0757866d9e4851f5fe47d737eb63fb2f7fd4fc3
Darien Moore <Darienmoore2002@gmail.com>
2023-03-28 23:19:07 -0500

    added missing type imports

--- weatherstackapi/types/__init__.py
+++ weatherstackapi/types/__init__.py
@@ -1,3 +1,10 @@
 from .response import Response
 from .error import Error
 from .enums import Units, Language
+from .astro import astro
+from .current import Current
+from .hourly import Hourly
+from .location import Location
+from .request import Request
+from .results import Result
+from .historical import Historical, Forecast
